885a030f98b0b66e559eb3a00673497a4d04a5de
sanlopez-wq <sanlopez@unal.edu.co>
2026-07-21 19:42:12 -0500

    Se corrigieron errores

--- proyecto.py
+++ proyecto.py
@@ -1,19 +1,78 @@
-def cargar_participantes():
+import random
+
+
+def guardar_participantes(participantes):
+    with open(participantes.txt, "r") as archivo:
+        for participante in participantes:
+            archivo.write(participante + "\n")
+
+def registrar_participantes():
+
     participantes = []
 
-    with open("participantes.txt", "r", encoding="utf-8") as archivo:
-        for linea in archivo:
-            participantes.append(linea.strip())
+    cantidad = int(input("Cantidad de participantes: "))
 
-    return participantes
-def mostrar_torneo(participantes, llaves):
+    for i in range(cantidad):
+        nombre = input(f"Participante {i + 1}: ")
+        participantes.append(nombre)
 
-    print("\nParticipantes")
 
-    for jugador in participantes:
-        print(jugador)
 
-    print("\nEnfrentamientos")
 
-    for pareja in llaves:
-        print(f"{pareja[0]} vs {pareja[1]}")
+def mostrar_participantes(participantes):
+
+    if len(participantes) == 0:
+        print("\nNo hay participantes registrados.\n")
+        return
+
+    print("\n Participantes :")
+
+    for i, participante in enumerate(participantes, start=1):
+        print(f"{i}. {participante}")
+
+    print()
+
+
+#Se generan enfrentamientos de forma aleatoria
+
+def generar_enfrentamientos(participantes):
+
+    random.shuffle(participantes)
+
+    enfrentamientos = []
+
+    for i in range(0, len(participantes), 2):
+        enfrentamientos.append(
+            [participantes[i], participantes[i + 1]]
+        )
+
+    return enfrentamientos
+
+
+def mostrar_llaves(enfrentamientos):
+
+    print("\n Enfrentamientos")
+
+    for i, duelo in enumerate(enfrentamientos, start=1):
+        print(f"Partido {i}: {duelo[0]} VS {duelo[1]}")
+
+    print()
+
+# Se simula aletoriamente
+def simular_ronda(enfrentamientos):
+
+    ganadores = []
+
+    print("\n Resultados")
+
+    for duelo in enfrentamientos:
+
+        ganador = random.choice(duelo)
+
+        print(f"{duelo[0]} VS {duelo[1]} -> Gana: {ganador}")
+
+        ganadores.append(ganador)
+
+    print()
+
+    return ganadores
