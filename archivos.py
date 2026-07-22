03a6ea78d977dbc069b1717ed0f9e48cdb76156b
jchavezl <jchavezl@unal.edu.co>
2026-07-21 21:23:27 -0500

    Completar archivos.py con las funciones para lectura de participantes, guardar e historial de campeones,

--- archivos.py
+++ archivos.py
@@ -3,23 +3,38 @@ RUTA_CAMPEONES = 'campeones.txt'
 
 
 def leer_participantes(ruta_archivo=RUTA_PARTICIPANTES):
-    lista=[]
-
-    archivo =open(ruta_archivo, 'r')
-
-    for linea in archivo:
-        linea= linea.strip()
-        if linea== '':
-            continue
-
-        datos = linea.split(',')
-        nombre = datos[0].strip()
-
-        if nombre == 'Nombre':
-            continue
-        lista.append(nombre)
-
-    archivo.close()
+    lista = []
+    try:
+        archivo = open(ruta_archivo, 'r')
+        for linea in archivo:
+            nombre = linea.strip()
+            if nombre != '':
+                lista.append(nombre)
+        archivo.close()
+    except FileNotFoundError:
+        print("No existe el archivo participantes.txt")
     return lista
 
 
+
+def guardar_campeon(nombre_campeon, ruta_archivo=RUTA_CAMPEONES):
+    archivo = open(ruta_archivo, 'a')
+    archivo.write(nombre_campeon + '\n')
+
+    archivo.close()
+
+
+
+def leer_historial(ruta_archivo=RUTA_CAMPEONES):
+    lista = []
+    try:
+        archivo = open(ruta_archivo, 'r')
+        for linea in archivo:
+            linea = linea.strip()
+            if linea != '':
+                lista.append(linea)
+        archivo.close()
+    except FileNotFoundError:
+        pass
+
+    return lista
